a6b1dd1bf5ebb89ab845557d5849867c23b7a470
Samira Toledo <samirat9ledo@gmail.com>
2026-02-03 15:57:02 -0300

    Update and rename Ex2.java to vetor-achar-par.java

--- vetor-achar-par.java
+++ vetor-achar-par.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ex2;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
 public class Ex2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         /*Escreva um algoritmo que Leia 10 valores inteiros e armazene-os em um Vetor. 
@@ -38,3 +28,4 @@ public class Ex2 {
     }
     
 }
+
